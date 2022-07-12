0f654fc9a0d4825da3749d20a9f12a3af1af9553
KAMAL-019 <kamalc2512btechaiml2025@kccitm.edu.in>
2022-07-12 12:41:10 +0530

    datatypes

---
+++ 3.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a = 10;
+    int b = 20;
+    int c = a + b;
+    cout <<"c is "<< c <<endl;
+    
+    char a1 = '*';
+    float a2 = 1.23;
+
+    cout << "char is " << a1<< endl;
+}
