a3c81bcfb389f13a6e904ddfd1b57c0a9829b68f
KAMAL-019 <kamalc2512btechaiml2025@kccitm.edu.in>
2022-07-12 12:51:53 +0530

    taking input

---
+++ 4.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a , b , c;
+    cout << "enter a and b" << endl;
+    cin >> a >> b ;
+    c = a + b;
+    cout << "c is " << c << endl;
+}
+
