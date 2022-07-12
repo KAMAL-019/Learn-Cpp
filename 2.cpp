464862f6d91b4da28f89f877773e250b8f0f1087
KAMAL-019 <kamalc2512btechaiml2025@kccitm.edu.in>
2022-07-12 12:23:39 +0530

    hello

---
+++ 2.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout <<"hello\nworld" <<endl<<"another line";
+    // endl use to end the line for the cout and print the next code on another line
+    // like \n
+}
